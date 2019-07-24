6d7ac9087fc12ee131cde1374ca42f7336d86c5f
Kevin <kevin.melan00@usc.edu.co>
2019-07-24 17:45:17 -0500

    alpha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SpotifyService } from './services/spotify.service';
+import { FormsModule } from '@angular/forms';
+import { CardSpotifyComponent } from './components/card-spotify/card-spotify.component';
+import { NoimagePipe } from './pipes/noimage.pipe';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent,
+    CardSpotifyComponent,
+    NoimagePipe,
+    LoadingComponent,
+    DomseguroPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
